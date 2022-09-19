0ebd40fd16a3364fde9e2c85005ed77dc1362963
CelvineAdiPutra <celvineadiputra2@gmail.com>
2022-09-19 15:58:34 +0700

    test mock api

--- test/restaurant_parsing_test.dart
+++ test/restaurant_parsing_test.dart
@@ -5,31 +5,31 @@ import 'package:crestaurant2/services/restaurant_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
 
-class MockClient extends Mock implements http.Client {}
+import 'restaurant_parsing_test.mocks.dart';
 
+@GenerateMocks([http.Client])
 void main() {
-  group("Test parsin json from api to restaurant model", () {
+  group("Test parsing json from api to restaurant model", () {
+    const mockData = {
+      "restaurants": [
+        {
+          "id": "s1knt6za9kkfw1e867",
+          "name": "Kafe Kita",
+          "description": "Quisque rutrum. Aenean imperdiet.",
+          "pictureId": "25",
+          "city": "Gorontalo",
+          "rating": 4
+        }
+      ]
+    };
+
     test("get list restaurant", () async {
       final client = MockClient();
 
-      const mockData = {
-        "restaurants" : [
-          {
-            "id": "s1knt6za9kkfw1e867",
-            "name": "Kafe Kita",
-            "description": "Quisque rutrum. Aenean imperdiet.",
-            "pictureId": "25",
-            "city": "Gorontalo",
-            "rating": 4
-          }
-        ]
-      };
-
       when(client.get(Uri.parse("https://restaurant-api.dicoding.dev/list")))
-          .thenAnswer(
-        (_) async => http.Response(jsonEncode(mockData), 200),
-      );
+          .thenAnswer((_) async => http.Response(jsonEncode(mockData), 200));
 
       expect(await RestaurantService().fetchData(), isA<List<Restaurant>>());
     });
